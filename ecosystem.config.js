1f0ae7cb59b29dc483723fd07eb478b3cd1dcf50
Seonghuichoe <heeday0410@gmail.com>
2019-01-18 21:00:32 +0900

    Update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       host: '35.247.34.197',
       ref: 'origin/master',
       repo: 'https://github.com/SeonghuiChoe/pm2-ecosystem-example.git',
-      path: '/home/seonghui/apps',
+      path: '/home/seonghui/apps/hello-api-server',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
     },
   },
